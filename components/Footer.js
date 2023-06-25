0fd14afef7b31294e8f717d3f3a724cdb5160ea4
Selim Ellieh <selim@Selims-MacBook-Pro.local>
2023-06-25 17:58:11 +0300

    Fixes

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
         <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <div class="sm:flex sm:items-center sm:justify-between">
                 <a href="#" class="flex items-center mb-4 sm:mb-0">
-                <Image class="mr-3" src="/heart_pulse_dark.svg" height={35} width={35} alt="Heart pump image" />
+                <Image class="mr-3" src="/heart_pulse.svg" height={35} width={35} alt="Heart pump image" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap text-primary">Gregoire ABI CHAKER</span>
                 </a>
                 <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-primary sm:mb-0">
