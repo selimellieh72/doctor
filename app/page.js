391bc7421c3f6995ede1f87d6ac32c9eda316c53
Selim Ellieh <selim@Selims-MBP.lan>
2023-06-27 20:00:15 +0300

    Fixes

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 
 
+
 export default function Home() {
 
 
 
   return (
    <main>
-        <section className="hero">
-        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <section className="hero my-8">
+        <div className="grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0  lg:grid-cols-12">
             <div className="text-center lg:mr-auto lg:text-left  place-self-center lg:col-span-7">
                 <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Gregoire Abi Chaker</h1>
                 <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Teismo medicinos ekspertas, gydytojas, turintis daugiau nei 20 metų patirties.</p>
@@ -17,10 +18,31 @@ export default function Home() {
                 </a> */}
             
             </div>
-            <div className="flex shadow md:w-1/2 md:mx-auto my-5 lg:w-full lg:my-0 lg:col-span-5">
+            <div className="flex shadow md:w-1/2 md:mx-auto  lg:w-full  lg:col-span-5">
                 <img className=" rounded-lg" src="/1.webp" alt="gregoire"/>
             </div>                
         </div>
+    </section>
+   
+    <div className=" text-center">
+        <span className=" rounded-full w-4 h-4 bg-secondary inline-block mx-1"/>
+        <span className=" rounded-full w-4 h-4 bg-secondary inline-block mx-1"/>
+        <span className=" rounded-full w-4 h-4 bg-secondary inline-block mx-1"/>
+        </div>
+    <section className="about my-8">
+        <div className=" text-center text-white font-bold text-4xl">
+            <h2>
+Apie mane</h2>
+        </div>
+        <div>
+            <p className=" font-light text-gray-400 py-4 px-4 lg:px-16 text-center">
+
+Teismo medicinos gydytojas <span className=" text-bold">Gregoire ABI CHAKER</span> yra nepriklausomas teismo medicinos ekspertas, privatus teismo ekspertas (įtrauktas į Lietuvos Respublikos teismo ekspertų sąrašą <a className=" text-blue-400" href="https://shorturl.at/aLST5">https://shorturl.at/aLST5</a>), turintis aukštąjį universitetinį medicininį išsilavinimą, teismo medicinos gydytojo specialybę, kvalifikaciją atlikti gyvų ir mirusių asmenų tyrimus, teismo medicinos gydytojo licenciją (Nr. MPL-09604). Darbo teismo medicinos srityje stažas nuo 1999 metų.
+Nepriklausomas teismo medicinos ekspertas konsultuoja visais teismo medicinos klausimais bei pateikia konsultacines išvadas (specialisto išvadas). Ekspertas yra kompetentingas, jo specialios žinios yra pagrįstos ilgamete darbo praktika, pasaulinės mokslinės literatūros, objektyvumo, supratingumo bei atidumo principais. Teismo medicinos gydytojas turi daugiau nei 20 metų darbo patirties Lietuvoje teismo medicinos srityje. Yra atlikęs daugiau nei 3 000 mirusiųjų tyrimų (autopsijų) bei daugiau nei 1 000 gyvų asmenų tyrimų.
+            </p>
+        </div>
+        
+
     </section>
      </main>
   )
--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,14 @@
 import Image from "next/image"
+
 export default function Footer() {
     return  <footer className="bg-secondary shadow absolute bottom-0 left-0 w-full">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <div className="sm:flex sm:items-center sm:justify-between">
-                <a href="#" className="flex items-center mb-4 sm:mb-0">
+                <a href="#" className="flex items-center justify-center mb-4 sm:mb-0">
                 <Image className="mr-3" src="/heart_pulse.svg" height={35} width={35} alt="Heart pump image" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-primary">Gregoire ABI CHAKER</span>
                 </a>
-                <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-primary sm:mb-0">
+                <ul className="flex flex-wrap items-center justify-center text-sm font-medium text-primary sm:mb-0">
                     <li>
                     <a href="#" className="mr-4 hover:underline md:mr-6 "><Image className="mr-3" src="/facebook_icon.svg" height={35} width={35} alt="Heart pump image" /></a>
                     </li>
@@ -17,8 +18,8 @@ export default function Footer() {
 
                 </ul>
             </div>
-            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span className="flex items-center align text-sm text-primary sm:text-center">Tel: +370 640 36369. El. paštas: teismomedicina.gac@gmail.com</span>
+            <hr className=" border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span className="flex text-xs lg:text-sm items-center align text-primary sm:text-center whitespace-nowrap mt-4">Tel: +370 640 36369. El. paštas: teismomedicina.gac@gmail.com</span>
         </div>
     </footer>
     
