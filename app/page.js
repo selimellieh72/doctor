6ce656300ac4b3baffb3acdc324ca41f0f9c5b42
Selim Ellieh <selim@Selims-MBP.lan>
2023-06-30 19:12:31 +0300

    Fixes

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0  lg:grid-cols-12">
             <div className="text-center lg:mr-auto lg:text-left  place-self-center lg:col-span-7">
                 <h1 className="max-w-2xl text-secondary mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Gregoire ABI CHAKER</h1>
-                <p className="max-w-2xl mb-6 font-light text-secondary lg:mb-8 md:text-lg lg:text-xl ">Teismo medicinos ekspertas, gydytojas, turintis daugiau nei 20 metų patirties.</p>
+                <p className="max-w-2xl mb-6 font-light text-secondary lg:mb-8 md:text-lg lg:text-xl">Teismo medicinos ekspertas, gydytojas, turintis daugiau nei 20 metų patirties.</p>
                 {/* <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-primary rounded-lg bg-secondary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     Get started
                     <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -21,10 +21,10 @@ export default function Home() {
         <span className=" rounded-full w-4 h-4 bg-secondary inline-block mx-1"/>
         <span className=" rounded-full w-4 h-4 bg-secondary inline-block mx-1"/>
         </div> */}
-                <p className="my-6 lg:my-8 lg:mr-6 font-semibold text-secondary text-left ">
+                <p className="my-6 lg:my-8 lg:mr-6 font-semibold text-secondary text-left lines leading-8">
 
 Teismo medicinos gydytojas <span className=" text-bold">Gregoire ABI CHAKER</span> yra nepriklausomas teismo medicinos ekspertas, privatus teismo ekspertas (įtrauktas į Lietuvos Respublikos teismo ekspertų sąrašą <a className=" text-blue-400" href="https://shorturl.at/aLST5">https://shorturl.at/aLST5</a>), turintis aukštąjį universitetinį medicininį išsilavinimą, teismo medicinos gydytojo specialybę, kvalifikaciją atlikti gyvų ir mirusių asmenų tyrimus, teismo medicinos gydytojo licenciją (Nr. MPL-09604). Darbo teismo medicinos srityje stažas nuo 1999 metų.
-Nepriklausomas teismo medicinos ekspertas konsultuoja visais teismo medicinos klausimais bei pateikia konsultacines išvadas (specialisto išvadas). Ekspertas yra kompetentingas, jo specialios žinios yra pagrįstos ilgamete darbo praktika, pasaulinės mokslinės literatūros, objektyvumo, supratingumo bei atidumo principais. Teismo medicinos gydytojas turi daugiau nei 20 metų darbo patirties Lietuvoje teismo medicinos srityje. Yra atlikęs daugiau nei 3 000 mirusiųjų tyrimų (autopsijų) bei daugiau nei 5 000 gyvų asmenų tyrimų.
+<br/>Nepriklausomas teismo medicinos ekspertas konsultuoja visais teismo medicinos klausimais bei pateikia konsultacines išvadas (specialisto išvadas). Ekspertas yra kompetentingas, jo specialios žinios yra pagrįstos ilgamete darbo praktika, pasaulinės mokslinės literatūros, objektyvumo, supratingumo bei atidumo principais. Teismo medicinos gydytojas turi daugiau nei 20 metų darbo patirties Lietuvoje teismo medicinos srityje. Yra atlikęs daugiau nei 3 000 mirusiųjų tyrimų (autopsijų) bei daugiau nei 5 000 gyvų asmenų tyrimų.
             </p>
             
             </div>
--- components/Header.js
+++ components/Header.js
@@ -11,21 +11,21 @@ export default function Header() {
 
 
   return (
-<header>
-      <div className='flex justify-between h-[66px] px-12 py-2 font-medium border-b-2 border-secondary text-center items-center bg-gray-100 opacity-100 hover:cursor-pointer hover:bg-white fixed w-full top-0 z-50'>
-      
-<span class>Profesionalios teismo medicinos paslaugos</span> <span className='font-semibold mx-2'>Tel. <span className='text-secondary font-bold'>+370 640 36369</span></span>
+<header className='px-12 py-4 lg:py-6 bg-secondary'>
+      <div className='hidden lg:flex justify-between bg-primary text-secondary py-2 px-4 mb-4'>
+      <span >Profesionalios teismo medicinos paslaugos</span> <span className='mx-2'>Tel. <span className='text-secondary'>+370 640 36369</span>
+      </span>
       </div>
-      <div className="flex flex-col lg:flex-row items-center justify-between px-12 py-6 bg-secondary">
+      <div className="flex flex-col lg:flex-row items-center justify-between ">
       
       <div className="flex items-center space-x-4">
       <Image src="/heart_pulse.svg" height={35} width={35} alt="Heart pump image" />
     
         <Link href="/" className=" font-bold text-lg text-primary">Gregoire ABI CHAKER</Link>
       </div>
-     
-      <ul className={`overflow-hidden transition-all ${isOpen? 'max-h-[350px]' : 'max-h-0' } lg:max-h-[350px] lg:pt-0 flex-col lg:flex-row lg:flex space-y-10 lg:space-y-0 lg:space-x-8 text-primary font-medium text-center`}>
-        <div/>
+      <span className='text-primary font-bold lg:hidden'>Tel. +370 640 36369</span>
+      <ul className={`overflow-hidden transition-all mt-4 lg:mt-0 ${isOpen? 'max-h-[350px]' : 'max-h-0' } lg:max-h-[350px] lg:pt-0 flex-col lg:flex-row lg:flex space-y-10 lg:space-y-0 lg:space-x-8 text-primary font-medium text-center`}>
+        
         <li className="hover:text-amber-700 transition-colors "><Link href="">Apie mane</Link></li>
         <li className="hover:text-amber-700 transition-colors"><Link href="">Gyvų asmenų tyrimai</Link></li>
         <li className="hover:text-amber-700 transition-cobzlors"><Link href="">Mirusių asmenų tyrimai</Link></li>
@@ -38,7 +38,7 @@ export default function Header() {
       </div>
       <div 
         onClick={() => setIsOpen(!isOpen)}
-      className="absolute top-[88px] right-4 flex lg:hidden align-center flex-col space-y-2 p-2 transition-opacity hover:cursor-pointer hover:opacity-80"
+      className="absolute top-[35px] right-4 flex lg:hidden align-center flex-col space-y-2 p-2 transition-opacity hover:cursor-pointer hover:opacity-80"
           
       >
        <span
