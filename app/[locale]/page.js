4bc81bb6fb30911c4b23b6769bf4f2f4d8863f46
Selim Ellieh <selim@Selims-MBP.lan>
2023-07-13 20:25:28 +0300

    Fixes

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Divider from "@/components/Divider"
 import { useTranslations } from "next-intl"
 
-import Link from "next/link"
+import Link from "next-intl/link"
 import { getTranslator } from "next-intl/server"
 
 export async function generateMetadata({ params: { locale } }) {
